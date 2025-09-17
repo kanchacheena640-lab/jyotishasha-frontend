cc90ac1c25b11ec183e95aa1ab81ca976fc0dfb2
Suvi Vish <kanchacheena640@gmail.com>
2025-09-17 08:44:21 +0530

    GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,40 @@ export const metadata = {
   title: "Jyotishasha",
   description: "Astrology Reports and Tools",
   icons: {
-    icon: "/favicon.ico", // 👈 yahan tumhara favicon add ho gaya
+    icon: "/favicon.ico",
   },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-WLP7T2DP'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WLP7T2DP');
+            `,
+          }}
+        />
+      </head>
       <body>
+        {/* ✅ Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WLP7T2DP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {/* Load Google Maps once for the whole app */}
         <Script
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&v=weekly`}
