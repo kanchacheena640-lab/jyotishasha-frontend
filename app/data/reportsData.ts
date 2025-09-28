162df8af3e7296f64a6da36f37fdafb5fc0e8349
Suvi Vish <kanchacheena640@gmail.com>
2025-09-28 22:59:43 +0530

    testing

--- app/data/reportsData.ts
+++ app/data/reportsData.ts
@@ -138,7 +138,7 @@ export const reportsData: Report[] = [
   {
     title: "Jupiter Transit Report",
     slug: "jupiter_transit_report",
-    price: 25,
+    price: 1,
     image: "/reports/jupiter_transit_report.webp",
     category: "Transit",
     description: "Know how Jupiter’s current transit impacts your career, finance, and relationships.",
